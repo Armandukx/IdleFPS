7ea6bc975232103d16160ff2004038e6ef89d73e
Armandukx <armandukx@users.noreply.github.com>
2026-04-03 18:23:11 +0200

    Up version

--- src/main/java/io/armandukx/idletweaks/IdleTweaks.java
+++ src/main/java/io/armandukx/idletweaks/IdleTweaks.java
@@ -14,7 +14,7 @@ import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.Formatting;
 
 public class IdleTweaks implements ClientModInitializer {
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.1.1";
     public static final String prefix =
             Formatting.YELLOW + "[I" + Formatting.GREEN + "D" + Formatting.RED + "T] " + Formatting.RESET;
     public static int renderDistance = 0;
